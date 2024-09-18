a36f4d5b843288c9fb6b7564625c097c50813171
KevinSilva <kevinsilvadb503@gmail.com>
2024-09-18 11:32:10 -0600

    entidad activo

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/Activo.cs
@@ -0,0 +1,16 @@
+﻿namespace Bodega_Api_Esfe_Agape.Models.EN
+{
+    public class Activo
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int estanteId { get; set; }
+        public int tipoActivoId { get; set; }
+        public string codigo { get; set; }
+        public string codigoBarra { get; set; }
+
+        //public estante estantes { get; set; }
+        //public tipoActivo tipoactivo { get; set; }
+    }
+}
