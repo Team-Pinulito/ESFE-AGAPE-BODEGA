825783492de7bdf406081300ecf83f6adc0f5480
KevinSilva <kevinsilvadb503@gmail.com>
2024-09-18 11:38:01 -0600

    Contexto

---
+++ ESFE AGAPE BODEGA.API/Models/DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Bodega_Api_Esfe_Agape.Models.EN;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+      : base(options)
+        {
+        }
+
+        public DbSet<Activo> activos { get; set; }
+        public DbSet<IngresoActivo> ingresoactivos { get; set; }
+        public DbSet<DetalleIngresoActivo> detalleIngresos { get; set; }
+    }
+}
