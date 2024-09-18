af56e3ce64037d33dd5bf55f4d4d459afd585284
KevinSilva <kevinsilvadb503@gmail.com>
2024-09-18 11:32:40 -0600

    entidad detalleingresoactivo

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/DetalleIngresoActivo.cs
@@ -0,0 +1,14 @@
+﻿namespace Bodega_Api_Esfe_Agape.Models.EN
+{
+    public class DetalleIngresoActivo
+    {
+        public int Id { get; set; }
+        public int ingresoActivoId { get; set; }
+        public int inventarioActivoId { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+
+        public IngresoActivo ingresoActivo { get; set; }
+        //public InventarioActivo inventarioactivo { get; set; }
+    }
+}
