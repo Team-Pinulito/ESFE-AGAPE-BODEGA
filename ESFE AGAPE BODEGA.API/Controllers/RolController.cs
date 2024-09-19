e0ac80baf976bd6e17f4aa05125596e71c7929ab
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-19 09:48:34 -0600

    Se creo el controlador de rol

--- ESFE AGAPE BODEGA.API/Controllers/RolController.cs
+++ ESFE AGAPE BODEGA.API/Controllers/RolController.cs
@@ -15,8 +15,82 @@ namespace ESFE_AGAPE_BODEGA.API.Controllers
         public RolController(RolDAL rolDAL)
         {
             _rolDAL = rolDAL;
+        
         }
 
+        //obtener todos
+        [HttpGet]
+        public async Task<List<BuscarRolResultadosDto.RolDto>> ObtenerTodos()
+        {
+            var roles = await _rolDAL.ObtenerRoles();
+
+            var rolesDto = roles.Select(r => new BuscarRolResultadosDto.RolDto
+            {
+                Id = r.Id,
+                Nombre = r.Nombre,
+                Descripcion = r.Descripcion
+            }).ToList();
+
+            return rolesDto;
+        }
+
+
+        //obtener todos paginados
+        [HttpPost("buscar")]
+        public async Task<BuscarRolResultadosDto> Buscar(BuscarRolFiltroDto rolDto)
+        {
+
+            var rol = new Rol
+            {
+                Nombre = rolDto.Nombre != null ? rolDto.Nombre : string.Empty,
+                Descripcion = rolDto.Descripcion != null ? rolDto.Descripcion : string.Empty
+            };
+
+            var roles = new List<Rol>();
+            var countRow = 0;
+
+            if (rolDto.SendRowCount == 2)
+            {
+                roles = await _rolDAL.BuscarPaginado(rol, skip: rolDto.Skip, take: rolDto.Take);
+                if (roles.Count > 0)
+                {
+                    countRow = await _rolDAL.ContarResultRol(rol);
+                }
+            }
+            else
+            {
+                roles = await _rolDAL.BuscarPaginado(rol, skip: rolDto.Skip, take: rolDto.Take);
+            }
+
+            var rolResult = new BuscarRolResultadosDto
+            {
+                Data = new List<BuscarRolResultadosDto.RolDto>(),
+                CountRow = countRow
+            };
+
+            foreach (var item in roles)
+            {
+                rolResult.Data.Add(new BuscarRolResultadosDto.RolDto
+                {
+                    Id = item.Id,
+                    Nombre = item.Nombre,
+                    Descripcion = item.Descripcion
+                });
+            }
+
+            return rolResult;
+
+        }
+
+        //obtener por id
+        [HttpGet("{id}")]
+        public async Task<Rol> ObtenerRolId(int id)
+        {
+            return await _rolDAL.ObtenerRolId(id);
+        }
+
+
+        //crear 
         [HttpPost]
         public async Task<IActionResult> Crear([FromBody] CrearRolDto crearRolDto)
         {
@@ -36,6 +110,50 @@ namespace ESFE_AGAPE_BODEGA.API.Controllers
             {
                 return StatusCode(500);
             }
+
         }
+
+        //actualizar
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Actualizar(int id, [FromBody] EditarRolDto editarRolDto)
+        {
+           var updateRol = await _rolDAL.ObtenerRolId(id);
+
+            if (updateRol == null)
+            {
+                return NotFound();
+            }
+
+            updateRol.Nombre = editarRolDto.Nombre;
+            updateRol.Descripcion = editarRolDto.Descripcion;
+
+            int result = await _rolDAL.ActualizarRol(updateRol);
+
+            if (result > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return StatusCode(500);
+            }   
+        }
+
+        //eliminar
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Eliminar(int id)
+        {
+            var result = await _rolDAL.EliminarRol(id);
+
+            if (result > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+        }
+
     }
 }
