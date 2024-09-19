01dde916c08960bb563d6ac35df89b985566733c
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-19 08:56:09 -0600

    Se creo el controlador de estante

---
+++ ESFE AGAPE BODEGA.API/Controllers/EstanteController.cs
@@ -0,0 +1,42 @@
+﻿using ESFE_AGAPE_BODEGA.API.Models.DAL;
+using ESFE_AGAPE_BODEGA.API.Models.Entitys;
+using ESFE_AGAPE_BODEGA.DTOs.EstanteDTOs;
+using ESFE_AGAPE_BODEGA.DTOs.RolDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE_AGAPE_BODEGA.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class EstanteController : Controller
+	{
+		private readonly EstanteDAL _estanteDAL;
+		public EstanteController(EstanteDAL estanteDAL)
+		{
+			_estanteDAL = estanteDAL;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Crear([FromBody] CrearEstanteDTO estanteDTO)
+		{
+			var estante = new Estante
+			{
+				Nombre = estanteDTO.Nombre,
+				Descripcion = estanteDTO.Descripcion,
+				BodegaId = estanteDTO.BodegaId
+			};
+
+			int result = await _estanteDAL.CrearEstante(estante);
+
+			if (result > 0)
+			{
+				return Ok(result);
+			}
+			else
+			{
+				return StatusCode(500);
+			}
+		}
+	}
+}
