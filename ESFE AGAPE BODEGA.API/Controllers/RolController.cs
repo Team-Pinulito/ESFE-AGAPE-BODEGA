af7da71d3060568b069088fb925b5dfd246df09d
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-18 22:30:07 -0600

    se creo el controlador para roles

---
+++ ESFE AGAPE BODEGA.API/Controllers/RolController.cs
@@ -0,0 +1,41 @@
+﻿using ESFE_AGAPE_BODEGA.API.Models.DAL;
+using ESFE_AGAPE_BODEGA.API.Models.Entitys;
+using ESFE_AGAPE_BODEGA.DTOs.RolDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE_AGAPE_BODEGA.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolController : ControllerBase
+    {
+
+        private readonly RolDAL _rolDAL;
+        public RolController(RolDAL rolDAL)
+        {
+            _rolDAL = rolDAL;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear([FromBody] CrearRolDto crearRolDto)
+        {
+            var rol = new Rol
+            {
+                Nombre = crearRolDto.Nombre,
+                Descripcion = crearRolDto.Descripcion
+            };
+
+            int result = await _rolDAL.CrearRol(rol);
+
+            if (result > 0)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
