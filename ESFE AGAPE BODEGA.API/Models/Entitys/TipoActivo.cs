4776c752a1b4d54e47837e67d33e9539d92ef258
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-09-19 08:00:11 -0600

    Se Agrego una referencia en el modelo TipoActivo

--- ESFE AGAPE BODEGA.API/Models/Entitys/TipoActivo.cs
+++ ESFE AGAPE BODEGA.API/Models/Entitys/TipoActivo.cs
@@ -1,19 +1,16 @@
 ﻿using Bodega_Api_Esfe_Agape.Models.EN;
-using System.ComponentModel.DataAnnotations;
 
 namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
 {
     public class TipoActivo
     {
-        [Key]
         public int Id { get; set; }
 
-        [Required]
         public string Nombre { get; set; }
 
         public string Descripcion { get; set; }
 
-        public Activo activo { get; set; }
+        public List<Activo> activo { get; set; }
 
     }
 }
