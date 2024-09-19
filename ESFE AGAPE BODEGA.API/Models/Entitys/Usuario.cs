9e5f6ed10c98181b4ec579a168764936ada237b9
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-18 21:29:38 -0600

    Modelo usuario

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+        public string DUI { get; set; }
+        public string Password { get; set; }
+        public string Codigo { get; set; }
+        public string Direccion { get; set; }
+    }
+}
