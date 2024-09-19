691c395d9fcd266618ee5e5de75c1a2aa654a4cd
KevinSilva <kevinsilvadb503@gmail.com>
2024-09-19 08:15:39 -0600

    Crear y editar DTO

---
+++ ESFE AGAPE BODEGA.DTOs/BodegaDTOs/CrearBodegaDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.BodegaDTOs
+{
+    public class CrearBodegaDTO
+    {
+        [Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+        [MaxLength(50, ErrorMessage = "El campo Nombre no puede tener más de 50 caracteres.")]
+        public string Nombre { get; set; }
+
+        [MaxLength(255, ErrorMessage = "El campo Descripción no puede tener más de 255 caracteres.")]
+        [Required(ErrorMessage = "La descripcion es obligatoria.")]
+        public string Descripcion { get; set; }
+    }
+}
