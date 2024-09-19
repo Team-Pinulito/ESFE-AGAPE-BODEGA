89d70a63c71ec76dfebe0bf2bf103746f85dac8f
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-18 21:24:03 -0600

    correciones

--- ESFE AGAPE BODEGA.API/Models/Entitys/Estante.cs
+++ ESFE AGAPE BODEGA.API/Models/Entitys/Estante.cs
@@ -5,17 +5,12 @@ namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
 	public class Estante
 	{
 	
-		[Key]
 		public int Id { get; set; }
 
-		[Required(ErrorMessage = "Bodega es obligatorio")]
-		[Display(Name = "Bodega")]
 		public int BodegaId { get; set; }
 
-		[Required(ErrorMessage = "El nombre es obligatorio.")]
 		public string Nombre { get; set; }
 
-		[Required(ErrorMessage = "La descripcion es obligatoria.")]
 		public string Descripcion { get; set; }
 
 
