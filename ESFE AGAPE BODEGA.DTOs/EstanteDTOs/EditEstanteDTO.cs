eccec7d82a9126ddca7c5b47ba26a8e739e02079
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-19 08:08:39 -0600

    Se creo el DTOs de Estante

---
+++ ESFE AGAPE BODEGA.DTOs/EstanteDTOs/EditEstanteDTO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.EstanteDTOs
+{
+	public class EditEstanteDTO
+	{
+		public EditEstanteDTO(GetIdResultEstanteDTO getIdResultEstanteDTO)
+		{
+			Id = getIdResultEstanteDTO.Id;
+			Nombre = getIdResultEstanteDTO.Nombre;
+			Descripcion = getIdResultEstanteDTO.Descripcion;
+		}
+		public EditEstanteDTO()
+		{
+			Nombre = string.Empty;
+		}
+
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+		[MaxLength(100, ErrorMessage = "El campo Nombre no puede tener más de 100 caracteres.")]
+		public string Nombre { get; set; }
+
+		[MaxLength(255, ErrorMessage = "El campo Descripción no puede tener más de 255 caracteres.")]
+		[Required(ErrorMessage = "La descripcion es obligatoria.")]
+		public string Descripcion { get; set; }
+	}
+}
---
+++ ESFE AGAPE BODEGA.DTOs/EstanteDTOs/GetIdResultEstanteDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.EstanteDTOs
+{
+	public class GetIdResultEstanteDTO
+	{
+		public int Id { get; set; }
+
+		public string Nombre { get; set; }
+
+		public string Descripcion { get; set; }
+	}
+}
