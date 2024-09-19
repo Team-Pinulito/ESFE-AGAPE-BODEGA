bafde79f65ea794ecaf884ebb91e8ff2a00ba4c5
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-19 10:18:45 -0600

    Se actualizo la dto de Estante

---
+++ ESFE AGAPE BODEGA.DTOs/EstanteDTOs/SearchResultEstanteDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.EstanteDTOs
+{
+	public class SearchResultEstanteDTO
+	{
+		public int CountRow { get; set; }
+		public List<EstanteDTO> Data { get; set; }
+		public class EstanteDTO
+		{
+			public int Id { get; set; }
+
+			[Display(Name = "Nombre")]
+			public string Nombre { get; set; }
+
+			[Display(Name = "Descripción")]
+			public string Descripcion { get; set; }
+
+			[Display(Name = "Bodega")]
+			public int BodegaId { get; set; }
+		}
+	}
+}
