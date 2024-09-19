339764a340d6c2045a256bb6683bcc1faa67309b
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-18 21:18:36 -0600

    Se creo la entidad de estante

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/Estante.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+{
+	public class Estante
+	{
+	
+		[Key]
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "Bodega es obligatorio")]
+		[Display(Name = "Bodega")]
+		public int BodegaId { get; set; }
+
+		[Required(ErrorMessage = "El nombre es obligatorio.")]
+		public string Nombre { get; set; }
+
+		[Required(ErrorMessage = "La descripcion es obligatoria.")]
+		public string Descripcion { get; set; }
+
+
+		public virtual Bodega Bodega { get; set; } = null!;
+	}
+}
