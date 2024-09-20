fdf24f6dabfbceb4d89e44d1eb5fbbc0fadd1650
KevinSilva <elmersilva462@gmail.com>
2024-09-20 17:35:05 -0600

    un error al enviar crear detalle ingreso activo DTOS por eso lo envio de nuevo

---
+++ ESFE AGAPE BODEGA.DTOs/DetalleInresoActivoDTOs/CrearDetalleIngresoActivoDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.DetalleIngresoActivo
+{
+    public class CrearDetalleIngresoActivoDTO
+    {
+        [Display(Name = "Inventario Activo")]
+        [Required(ErrorMessage = "El campo ActivoId es obligatorio.")]
+        public int InventarioActivoId { get; set; }
+
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        public decimal Precio { get; set; }
+    }
+}
