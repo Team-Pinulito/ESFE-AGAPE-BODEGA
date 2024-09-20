57850004aabac59b216b63da42dc33ad7e9f3b6a
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-20 15:45:49 -0600

    fix bug

---
+++ ESFE AGAPE BODEGA.API/Migrations/20240920214220_20240920_3.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESFE_AGAPE_BODEGA.API.Migrations
+{
+    public partial class _20240920_3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_solicitudActivos_usuarios_UsuarioId",
+                table: "solicitudActivos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_solicitudActivos_usuarios_UsuarioId",
+                table: "solicitudActivos",
+                column: "UsuarioId",
+                principalTable: "usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_solicitudActivos_usuarios_UsuarioId",
+                table: "solicitudActivos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_solicitudActivos_usuarios_UsuarioId",
+                table: "solicitudActivos",
+                column: "UsuarioId",
+                principalTable: "usuarios",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
