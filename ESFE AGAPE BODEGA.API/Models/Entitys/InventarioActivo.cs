541444eb01a37c5c45d160bfbdd64cd6a579ca02
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-20 12:14:54 -0600

    Se agrega modelo InventrarioActivo

--- ESFE AGAPE BODEGA.API/Models/Entitys/InventarioActivo.cs
+++ ESFE AGAPE BODEGA.API/Models/Entitys/InventarioActivo.cs
@@ -1,11 +1,14 @@
 ﻿using Bodega_Api_Esfe_Agape.Models.EN;
+using System.ComponentModel.DataAnnotations;
 
 namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
 {
     public class InventarioActivo
     {
         public int Id { get; init; }
+        [Display(Name = "Activo")]
         public Activo ActivoId { get; init; }
+        [Display(Name = "Estante")]
         public Estante EstanteId { get; init; }
         public int Existencia { get; init; }
 
