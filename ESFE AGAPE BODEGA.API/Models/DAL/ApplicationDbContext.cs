054e39b30711e6b3c333184ac01d57c1a10e663e
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-19 21:19:32 -0600

    Se añadio la eliminacion en casada de paquete activo en el dbContext

--- ESFE AGAPE BODEGA.API/Models/DAL/ApplicationDbContext.cs
+++ ESFE AGAPE BODEGA.API/Models/DAL/ApplicationDbContext.cs
@@ -4,22 +4,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ESFE_AGAPE_BODEGA.API.Models.DAL
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-      : base(options)
-        {
-        }
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+	  : base(options)
+		{
+		}
 
-        public DbSet<Activo> activos { get; set; }
+		public DbSet<Activo> activos { get; set; }
 		public DbSet<Estante> estantes { get; set; }
 		public DbSet<IngresoActivo> ingresoactivos { get; set; }
-        public DbSet<DetalleIngresoActivo> detalleIngresos { get; set; }
-        public DbSet<TipoActivo> tipoActivos { get; set; }
-        public DbSet<Bodega> bodegas { get; set; }
-        public DbSet<Rol> roles { get; set; }
-        public DbSet<Usuario> usuarios { get; set; }
+		public DbSet<DetalleIngresoActivo> detalleIngresos { get; set; }
+		public DbSet<TipoActivo> tipoActivos { get; set; }
+		public DbSet<Bodega> bodegas { get; set; }
+		public DbSet<Rol> roles { get; set; }
+		public DbSet<Usuario> usuarios { get; set; }
 		public DbSet<PaqueteActivo> paqueteActivos { get; set; }
 		public DbSet<DetallePaqueteActivo> detallePaqueteActivos { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			modelBuilder.Entity<PaqueteActivo>()
+			.HasMany(p => p.DetallePaqueteActivos)
+			.WithOne(d => d.PaqueteActivo)
+			.HasForeignKey(d => d.PaqueteActivoId)
+			.OnDelete(DeleteBehavior.Cascade); // Aquí habilitas la eliminación en cascada
+		}
 	}
 }
