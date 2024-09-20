75b8043e523d2607e5b734361f4a569ef83e14ac
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-20 11:45:17 -0600

    Se añadio la clase DetallePaqueteActivoDTO

---
+++ ESFE AGAPE BODEGA.DTOs/DetallePaqueteActivoDTOs/DetallePaqueteActivoDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs
+{
+	public class DetallePaqueteActivoDTO
+	{
+
+		public int Id { get; set; }
+
+		[Display(Name = "Activo")]
+		[Required(ErrorMessage = "El campo ActivoId es obligatorio.")]
+		public int ActivoId { get; set; }
+
+		[Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+		public int Cantidad { get; set; }
+	}
+}
+}
