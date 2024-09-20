45bc89206e05600399d790cd0202d7d9d3fdab41
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-20 13:53:36 -0600

    Añadir clase DetalleSolicitudActivo con propiedades
    Se ha añadido una nueva clase `DetalleSolicitudActivo` en el archivo `DetalleSolicitudActivo.cs` con las propiedades `Id`, `SolicitudActivoId`, `ActivoId`, `PaqueteActivoId`, `Cantidad` y `Status`. También se han añadido propiedades de navegación para `Activo`, `PaqueteActivo` y `SolicitudActivo`. Además, se ha importado el espacio de nombres `Bodega_Api_Esfe_Agape.Models.EN`.

--- ESFE AGAPE BODEGA.API/Models/Entitys/DetalleSolicitudActivo.cs
+++ ESFE AGAPE BODEGA.API/Models/Entitys/DetalleSolicitudActivo.cs
@@ -1,6 +1,20 @@
-﻿namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+﻿using Bodega_Api_Esfe_Agape.Models.EN;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
 {
     public class DetalleSolicitudActivo
     {
+        public int Id { get; set; }
+        public int SolicitudActivoId { get; set; }
+        public int ActivoId { get; set; }
+
+        public int PaqueteActivoId { get; set; }
+        public int Cantidad { get; set; }
+        public byte Status { get; set; }
+
+        public Activo Activo { get; set; }
+        public PaqueteActivo PaqueteActivo { get; set; }
+        public SolicitudActivo SolicitudActivo { get; set; }
+
     }
 }
