2b5c949dae8f3e66d658165186196d404e3498d1
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-20 11:47:41 -0600

    Añadir GetIdResultPaqueteActivoDTO.cs con nuevas propiedades
    Se ha añadido un nuevo archivo `GetIdResultPaqueteActivoDTO.cs`.
    Se han importado los espacios de nombres necesarios.
    Se ha creado el espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.PaqueteActivoDTOs`.
    Dentro de este espacio de nombres, se ha definido la clase `GetIdResultPaqueteActivoDTO`.
    La clase contiene las propiedades `Id`, `Correlativo`, `Nombre` y `DetallePaqueteActivos`.

---
+++ ESFE AGAPE BODEGA.DTOs/PaqueteActivoDTOs/GetIdResultPaqueteActivoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.PaqueteActivoDTOs
+{
+	public class GetIdResultPaqueteActivoDTO
+	{
+		public int Id { get; set; }
+
+		public string Correlativo { get; set; }
+
+		public string Nombre { get; set; }
+
+		public List<DetallePaqueteActivoDTO> DetallePaqueteActivos { get; set; }
+	}
+}
