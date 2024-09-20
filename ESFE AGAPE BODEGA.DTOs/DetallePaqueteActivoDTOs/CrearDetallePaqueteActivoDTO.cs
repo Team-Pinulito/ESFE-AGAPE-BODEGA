635b9aad4d604d2cd96c9cffbb83cea099802a1c
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-20 15:31:36 -0600

    Añadir CrearDetallePaqueteActivoDTO.cs con validaciones
    Se ha añadido un nuevo archivo llamado `CrearDetallePaqueteActivoDTO.cs` que incluye la definición de la clase `CrearDetallePaqueteActivoDTO` dentro del espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs`. Esta clase contiene dos propiedades: `ActivoId` y `Cantidad`, ambas con atributos de validación para asegurar que los campos sean obligatorios y mostrar mensajes de error adecuados.

---
+++ ESFE AGAPE BODEGA.DTOs/DetallePaqueteActivoDTOs/CrearDetallePaqueteActivoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs
+{
+	public class CrearDetallePaqueteActivoDTO
+	{
+
+		[Display(Name = "Activo")]
+		[Required(ErrorMessage = "El campo ActivoId es obligatorio.")]
+		public int ActivoId { get; set; }
+
+		[Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+		public int Cantidad { get; set; }
+	}
+}
