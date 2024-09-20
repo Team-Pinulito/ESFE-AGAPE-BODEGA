20c87fda5c58034f02607b4f1622602065e2ef77
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-20 11:48:04 -0600

    Añadir SearchResultPaqueteActivoDTO y clases internas
    Se ha añadido un nuevo archivo `SearchResultPaqueteActivoDTO.cs` que contiene la definición de la clase `SearchResultPaqueteActivoDTO`. Esta clase incluye dos propiedades: `CountRow`, que es un entero, y `Data`, que es una lista de objetos `PaqueteActivoDTO`.
    
    Dentro de `SearchResultPaqueteActivoDTO`, se ha definido una clase interna `PaqueteActivoDTO` con las siguientes propiedades:
    - `Id`: un entero que representa el identificador del paquete.
    - `Correlativo`: una cadena con un atributo de visualización (`Display`) que muestra "Correlativo".
    - `Nombre`: una cadena con un atributo de visualización (`Display`) que muestra "Nombre".
    - `DetallePaqueteActivos`: una lista de objetos `DetallePaqueteActivoDTO`.
    
    También se han añadido las directivas `using` necesarias para importar los espacios de nombres requeridos.

---
+++ ESFE AGAPE BODEGA.DTOs/PaqueteActivoDTOs/SearchResultPaqueteActivoDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.PaqueteActivoDTOs
+{
+	public class SearchResultPaqueteActivoDTO
+	{
+		public int CountRow { get; set; }
+		public List<PaqueteActivoDTO> Data { get; set; }
+
+		public class PaqueteActivoDTO
+		{
+			public int Id { get; set; }
+
+			[Display(Name = "Correlativo")]
+			public string Correlativo { get; set; }
+
+			[Display(Name = "Nombre")]
+			public string Nombre { get; set; }
+
+			public List<DetallePaqueteActivoDTO> DetallePaqueteActivos { get; set; }
+		}
+	}
+}
