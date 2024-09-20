0f71b9ea1b3a05f322f836aa0e2b8cc26b3b15d8
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-20 14:05:20 -0600

    Añadir clase SolicitudActivo con nuevas propiedades
    Se ha añadido una nueva clase `SolicitudActivo` en el archivo `SolicitudActivo.cs` dentro del espacio de nombres `ESFE_AGAPE_BODEGA.API.Models.Entitys`. La clase incluye propiedades como `Id`, `UsuarioId`, `Correlativo`, `Fecha`, entre otras. También se han añadido propiedades de navegación para las relaciones con otras entidades y una colección `DetalleSolicitudActivos` para manejar una relación de uno a muchos con la entidad `DetalleSolicitudActivo`.

--- ESFE AGAPE BODEGA.API/Models/Entitys/SolicitudActivo.cs
+++ ESFE AGAPE BODEGA.API/Models/Entitys/SolicitudActivo.cs
@@ -2,5 +2,27 @@
 {
     public class SolicitudActivo
     {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public int UsuarioIdBodegueroEntregaId { get; set; }
+        public int UsuarioIdBodegueroRecibeId { get; set; }
+        public string Correlativo { get; set; }
+        public DateTime Fecha { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public DateTime FechaRecepcion { get; set; }
+        public DateTime FechaEntregaEsperada { get; set; }
+        public DateTime FechaRecepcionEsperada { get; set; }
+        public string Comentario { get; set; }
+
+        //para UsuarioId
+        public Usuario Usuario { get; set; }
+
+        //para UsuarioIdBodegueroEntregaId
+        public Usuario UsuarioBodegueroEntrega { get; set; }
+
+        //para UsuarioIdBodegueroRecibeId
+        public Usuario UsuarioBodegueroRecibe { get; set; }
+
+        public ICollection<DetalleSolicitudActivo> DetalleSolicitudActivos { get; set; }
     }
 }
