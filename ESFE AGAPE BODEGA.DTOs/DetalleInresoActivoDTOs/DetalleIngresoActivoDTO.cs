456c62af9300950f89801b4d3e2d64c0719ce6c5
KevinSilva <elmersilva462@gmail.com>
2024-09-20 17:37:13 -0600

    Detalle Ingreso Activo DTOs

---
+++ ESFE AGAPE BODEGA.DTOs/DetalleInresoActivoDTOs/DetalleIngresoActivoDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.DetalleInresoActivoDTOs
+{
+    public class DetalleIngresoActivoDTO
+    {
+        public int Id { get; set; }
+        [Display(Name = "Inventario Activo")]
+        [Required(ErrorMessage = "El campo ActivoId es obligatorio.")]
+        public int InventarioActivoId { get; set; }
+
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        public decimal Precio { get; set; }
+    }
+}
