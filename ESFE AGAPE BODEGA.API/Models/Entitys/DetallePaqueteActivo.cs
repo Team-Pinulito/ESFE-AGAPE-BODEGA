212990ac781b2102d199bc20535ca1b1daa1ff39
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-19 20:27:16 -0600

    Se creo la entidad DetallePaqueteActivo

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/DetallePaqueteActivo.cs
@@ -0,0 +1,18 @@
+﻿using Bodega_Api_Esfe_Agape.Models.EN;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+{
+	public class DetallePaqueteActivo
+	{
+		public int Id { get; set; }
+
+		public int ActivoId { get; set; }
+		public int PaqueteActivoId { get; set; }
+
+		public int Cantidad { get; set; }
+
+		public Activo Activo { get; set; }
+
+		public PaqueteActivo PaqueteActivo { get; set; }
+	}
+}
