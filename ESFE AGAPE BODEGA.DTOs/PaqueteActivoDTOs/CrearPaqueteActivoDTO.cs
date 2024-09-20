056a8594bd632ed946f4916de8e08d01ce701dca
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-09-20 11:47:13 -0600

    Añadir CrearPaqueteActivoDTO con propiedades y constructor
    Se ha añadido un nuevo archivo llamado `CrearPaqueteActivoDTO.cs` que incluye:
    - Importación de varios espacios de nombres necesarios.
    - Creación del espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.PaqueteActivoDTOs`.
    - Definición de la clase `CrearPaqueteActivoDTO`.
    - Constructor que inicializa `DetallePaqueteActivos` como una lista de `DetallePaqueteActivoDTO`.
    - Propiedades `Correlativo` (cadena requerida, máx. 50 caracteres) y `Nombre` (cadena requerida, máx. 100 caracteres).
    - Propiedad `DetallePaqueteActivos` como lista de `DetallePaqueteActivoDTO`.

---
+++ ESFE AGAPE BODEGA.DTOs/PaqueteActivoDTOs/CrearPaqueteActivoDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.PaqueteActivoDTOs
+{
+	public class CrearPaqueteActivoDTO
+	{
+		public CrearPaqueteActivoDTO()
+		{
+			DetallePaqueteActivos = new List<DetallePaqueteActivoDTO>();
+		}
+
+		[Required(ErrorMessage = "El campo Correlativo es obligatorio.")]
+		[MaxLength(50, ErrorMessage = "El campo Correlativo no puede tener más de 50 caracteres.")]
+		public string Correlativo { get; set; }
+
+		[Required(ErrorMessage = "El campo Nombre es obligatorio.")]
+		[MaxLength(100, ErrorMessage = "El campo Nombre no puede tener más de 100 caracteres.")]
+		public string Nombre { get; set; }
+
+		public List<DetallePaqueteActivoDTO> DetallePaqueteActivos { get; set; }
+
+	}
+
+	
+		
+}
