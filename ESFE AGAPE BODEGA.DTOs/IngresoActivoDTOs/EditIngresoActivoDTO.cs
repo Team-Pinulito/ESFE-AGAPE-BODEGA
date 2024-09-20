dcdfe7a573c8bfdb36c98ff8b4a7d43fb3e26571
KevinSilva <elmersilva462@gmail.com>
2024-09-20 17:47:13 -0600

    Get id result ingreso actvio DTOs

---
+++ ESFE AGAPE BODEGA.DTOs/IngresoActivoDTOs/EditIngresoActivoDTO.cs
@@ -0,0 +1,20 @@
+﻿using ESFE_AGAPE_BODEGA.DTOs.IngresoActivoDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.IngresoActivoDTOs
+{
+    public class EditIngresoActivoDTO
+    {
+        public EditIngresoActivoDTO(GetIdResultIngresoActivoDTO getIdResultIngresoActivoDTO)
+        {
+            Id = getIdResultIngresoActivoDTO.Id;
+            Correlativo = getIdResultIngresoActivoDTO.Correlativo;
+            Nombre = getIdResultPaqueteActivoDTO.Nombre;
+            DetallePaqueteActivos = getIdResultPaqueteActivoDTO.DetallePaqueteActivos;
+        }
+    }
+}
---
+++ ESFE AGAPE BODEGA.DTOs/IngresoActivoDTOs/GetIdResultIngresoActivoDTO.cs
@@ -0,0 +1,23 @@
+﻿using ESFE_AGAPE_BODEGA.DTOs.DetalleInresoActivoDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ESFE_AGAPE_BODEGA.DTOs.UsuarioDTOs.GetIdResultUsuarioDTO;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.IngresoActivoDTOs
+{
+    public class GetIdResultIngresoActivoDTO
+    {
+        public int Id { get; set; }
+        public int UsuarioId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public string Correlativo { get; set; }
+        public string NumeroDocRelacionado { get; set; }
+        public decimal Total { get; set; }
+
+        public UsuarioDTO Usuario { get; set; }
+        public IList<DetalleIngresoActivoDTO> detalleIngresoActivoDTOs { get; set; }
+    }
+}
