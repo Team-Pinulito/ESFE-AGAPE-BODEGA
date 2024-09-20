fce64a31f43ed6f723b4b78db16afc45e41ba51d
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-19 21:36:58 -0600

    Se suben modelos AjusteInventario

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/AjusteInventario.cs
@@ -0,0 +1,16 @@
+﻿namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+{
+    public class AjusteInventario
+    {
+        public int Id { get; set; }
+        public Usuario UsuarioId { get; set; }
+        public DateTime FechaIngreso { get; set; }
+        public int Correlativo { get; set; }
+        public int Cantidad { get; set; }
+        public int TipoMantenimiento { get; set; }
+        public string Comentario { get; set; }
+
+        public List<InventarioActivo> InventarioActivos { get; set; }
+
+    }
+}
