d1e07e48e812047d273c6c80390f432980b5a28d
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-09-24 12:46:55 -0600

    se crearon las DTOs de KardexActivo

---
+++ ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/SearchQueryKardexActivoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.KardexActivoDTOs
+{
+    public class SearchQueryKardexActivoDTO
+    {
+        [Display(Name = "Inventario Activo")]
+        public int? InventarioActivoId { get; set; }
+
+        [Display(Name = "Fecha Movimiento")]
+        public DateTime? FechaMovimiento { get; set; }
+
+        public int Skip { get; set; }
+        public int Take { get; set; }
+    }
+}
---
+++ ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/SearchResultKardexActivoDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace ESFE_AGAPE_BODEGA.DTOs.KardexActivoDTOs
+{
+    public class SearchResultKardexActivoDTO
+    {
+        public int CountRow { get; set; }
+        public List<KardexActivoDTO> Data { get; set; }
+
+        public class KardexActivoDTO
+        {
+            public int Ids { get; set; }
+            public int InventarioActivo { get; set; }
+            public DateTime FechaMovimiento { get; set; }
+            public int Cantidad { get; set; }
+            public byte TipoMovimiento { get; set; }
+            public DateTime Saldo { get; set; }
+        }
+
+    }
+}
