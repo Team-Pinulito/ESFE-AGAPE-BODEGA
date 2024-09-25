e2f3173c4c2c4da4d5b5c25c597c5109e988634f
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-09-25 10:39:28 -0600

    se modifico el DTO CreateKardexActivo

--- ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/CreateKardexActivoDTO.cs
+++ ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/CreateKardexActivoDTO.cs
@@ -6,19 +6,18 @@ namespace ESFE_AGAPE_BODEGA.DTOs.KardexActivoDTOs
     public class CreateKardexActivoDTO
     {
         [DisplayName("Usuario")]
-        [Required(ErrorMessage = "El campo Usuario es obligatorio.")]
+        [Required(ErrorMessage = "El campo InventarioActivoId es obligatorio.")]
         public int InventarioActivoId { get; set; }
-        [DisplayName("Fecha de Ingreso")]
-        [Required(ErrorMessage = "El campo Fecha de Ingreso es obligatorio.")]
+        [DisplayName("Fecha de Movimiento")]
+        [Required(ErrorMessage = "El campo Fecha de Movimiento es obligatorio.")]
         public DateTime FechaMovimiento { get; set; }
-        [Required(ErrorMessage = "El campo Correlativo es obligatorio.")]
-        [Range(1, int.MaxValue, ErrorMessage = "El Correlativo debe ser mayor que cero.")]
-        public int Cantidad { get; set; }
         [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
-        [Range(1, int.MaxValue, ErrorMessage = "La Cantidad debe ser mayor que cero.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El Cantidad debe ser mayor que cero.")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El campo TipoMovimiento es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La TipoMovimiento debe ser mayor que cero.")]
         public byte TipoMovimiento { get; set; }
-        [Required(ErrorMessage = "El campo TipoMantenimiento es obligatorio.")]
-        [Range(1, int.MaxValue, ErrorMessage = "El TipoMantenimiento debe ser mayor que cero.")]
+        [Required(ErrorMessage = "El campo Saldo es obligatorio.")]
         public DateTime Saldo { get; set; }
     }
 }
