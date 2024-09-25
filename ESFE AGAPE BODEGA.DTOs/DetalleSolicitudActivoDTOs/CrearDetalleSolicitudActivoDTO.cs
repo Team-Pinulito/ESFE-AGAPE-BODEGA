7fe141f9d1b44364677f12686a6dd4d7fd165e6d
Juan David Molina Ruano <juandavidmolinaruano48@gmail.com>
2024-09-24 22:31:23 -0600

    Añadir DTOs para DetalleSolicitudActivo
    Se han añadido dos nuevos archivos de código fuente: `CrearDetalleSolicitudActivoDTO.cs` y `DetalleSolicitudActivoDTO.cs`.
    
    En `CrearDetalleSolicitudActivoDTO.cs`:
    - Nueva clase `CrearDetalleSolicitudActivoDTO` en el espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.DetalleSolicitudActivoDTOs`.
    - Propiedades: `ActivoId`, `PaqueteActivoId`, `Cantidad` (requerido), `Status`.
    
    En `DetalleSolicitudActivoDTO.cs`:
    - Nueva clase `DetalleSolicitudActivoDTO` en el espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.DetalleSolicitudActivoDTOs`.
    - Propiedades: `Id`, `ActivoId`, `PaqueteActivoId`, `Cantidad` (requerido), `Status`.

---
+++ ESFE AGAPE BODEGA.DTOs/DetalleSolicitudActivoDTOs/CrearDetalleSolicitudActivoDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.DetalleSolicitudActivoDTOs
+{
+    public class CrearDetalleSolicitudActivoDTO
+    {
+        [Display(Name = "Activo")]
+        public int ActivoId { get; set; }
+
+        [Display(Name = "Paquete Activo")]
+        public int PaqueteActivoId { get; set; }
+
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        public int Cantidad { get; set; }
+
+        [Display(Name = "Estado")]
+        public byte Status { get; set; }
+    }
+}
