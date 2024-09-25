e2beba44ebfa357788cc2a359916330dccdb33fe
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-24 22:47:32 -0600

    Se modifican

--- ESFE AGAPE BODEGA.BlazorWebAssembly/Program.cs
+++ ESFE AGAPE BODEGA.BlazorWebAssembly/Program.cs
@@ -1,3 +1,5 @@
+using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 using ESFE_AGAPE_BODEGA.BlazorWebAssembly;
 using ESFE_AGAPE_BODEGA.BlazorWebAssembly.DataService;
 using Microsoft.AspNetCore.Components.Web;
@@ -11,12 +13,23 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 
 builder.Services.AddHttpClient("BodegaAPI", client =>
 {
-    client.BaseAddress = new Uri("https://bodega.bsite.net/api/");
+    client.BaseAddress = new Uri("https://localhost:7214/api/");
 });
 
 builder.Services.AddScoped<RolService>();
 
 builder.Services.AddScoped<UsuarioService>();
+
+builder.Services.AddScoped<AuthService>();
+
+builder.Services.AddBlazoredSessionStorage();
+
+builder.Services.AddTransient<AuthTokenHandler>();
+
+builder.Services.AddHttpClient("BodegaAPI")
+    .AddHttpMessageHandler<AuthTokenHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("BodegaAPI"));
 
 await builder.Build().RunAsync();
 
