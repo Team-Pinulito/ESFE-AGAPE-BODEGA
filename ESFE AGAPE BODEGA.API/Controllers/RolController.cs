7a157235d65cdcc2e3a4e5e09f05e4b5301ef774
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-24 22:15:07 -0600

    Se agrega autenticate a rol

--- ESFE AGAPE BODEGA.API/Controllers/RolController.cs
+++ ESFE AGAPE BODEGA.API/Controllers/RolController.cs
@@ -3,11 +3,13 @@ using ESFE_AGAPE_BODEGA.API.Models.Entitys;
 using ESFE_AGAPE_BODEGA.DTOs.EstanteDTOs;
 using ESFE_AGAPE_BODEGA.DTOs.RolDTOs;
 using ESFE_AGAPE_BODEGA.DTOs.UsuarioDTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ESFE_AGAPE_BODEGA.API.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class RolController : ControllerBase
