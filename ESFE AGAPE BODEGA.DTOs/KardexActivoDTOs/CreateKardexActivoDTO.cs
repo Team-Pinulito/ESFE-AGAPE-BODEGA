8123b4a6f84f37782068ddf568b32741a1ab14ce
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-09-25 10:01:05 -0600

    Se crearon las DTOs Create y GetId de KardexActivo

---
+++ ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/CreateKardexActivoDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.KardexActivoDTOs
+{
+    public class CreateKardexActivoDTO
+    {
+        [DisplayName("Usuario")]
+        [Required(ErrorMessage = "El campo Usuario es obligatorio.")]
+        public int InventarioActivoId { get; set; }
+        [DisplayName("Fecha de Ingreso")]
+        [Required(ErrorMessage = "El campo Fecha de Ingreso es obligatorio.")]
+        public DateTime FechaMovimiento { get; set; }
+        [Required(ErrorMessage = "El campo Correlativo es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El Correlativo debe ser mayor que cero.")]
+        public int Cantidad { get; set; }
+        [Required(ErrorMessage = "El campo Cantidad es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La Cantidad debe ser mayor que cero.")]
+        public byte TipoMovimiento { get; set; }
+        [Required(ErrorMessage = "El campo TipoMantenimiento es obligatorio.")]
+        [Range(1, int.MaxValue, ErrorMessage = "El TipoMantenimiento debe ser mayor que cero.")]
+        public DateTime Saldo { get; set; }
+    }
+}
---
+++ ESFE AGAPE BODEGA.DTOs/KardexActivoDTOs/GetIdResultKardexActivoDTO.cs
@@ -0,0 +1,18 @@
+﻿using static ESFE_AGAPE_BODEGA.DTOs.InventarioActivoDTOs.GetIdResultInventarioActivoDTO;
+
+
+namespace ESFE_AGAPE_BODEGA.DTOs.KardexActivoDTOs
+{
+    public class GetIdResultKardexActivoDTO
+    {
+        public int Id { get; set; }
+        public int InventarioActivoId { get; set; }
+        public DateTime FechaMovimiento { get; set; }
+        public int Cantidad { get; set; }
+        public byte TipoMovimiento { get; set; }
+        public DateTime Saldo { get; set; }
+
+        public InventarioActivoDTO InventarioActivo { get; set; }
+        public List<InventarioActivoDTO> InventarioActivos { get; set; }
+    }
+}
