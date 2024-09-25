574e6606e2bdb6afbc7c4876471e2c58fed29e68
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-09-24 22:46:19 -0600

    Se agrega AuthToken

---
+++ ESFE AGAPE BODEGA.BlazorWebAssembly/DataService/AuthTokenHandler.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using Blazored.SessionStorage;
+using System.Net.Http.Headers;
+
+namespace ESFE_AGAPE_BODEGA.BlazorWebAssembly.DataService
+{
+    public class AuthTokenHandler : DelegatingHandler
+    {
+        private readonly ISessionStorageService _localStorage;
+
+        public AuthTokenHandler(ISessionStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var token = await _localStorage.GetItemAsync<string>("authToken");
+
+            if (!string.IsNullOrWhiteSpace(token))
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
