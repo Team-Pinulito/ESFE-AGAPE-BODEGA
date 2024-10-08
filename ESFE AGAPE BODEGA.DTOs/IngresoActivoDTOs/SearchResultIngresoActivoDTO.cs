1004786300ced915703abbdfdd876ee1e98c3a07
Diego-Edenilson-Martinez-Lopez2004 <diego.edenilson.martinez.lopez.n25@gmail.com>
2024-10-08 11:18:35 -0600

    se crearon los componentes Editar, Eliminar y Lista de IngresoActivo

--- ESFE AGAPE BODEGA.DTOs/IngresoActivoDTOs/SearchResultIngresoActivoDTO.cs
+++ ESFE AGAPE BODEGA.DTOs/IngresoActivoDTOs/SearchResultIngresoActivoDTO.cs
@@ -1,29 +1,26 @@
 ﻿using ESFE_AGAPE_BODEGA.DTOs.DetalleInresoActivoDTOs;
-using ESFE_AGAPE_BODEGA.DTOs.DetallePaqueteActivoDTOs;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace ESFE_AGAPE_BODEGA.DTOs.IngresoActivoDTOs
 {
-    public class SearchResultIngresoActivoDTO
-    {
-        public int CountRow { get; set; }
-        public List<IngresoActivoDTO> Data { get; set; }
+	public class SearchResultIngresoActivoDTO
+	{
+		public int CountRow { get; set; }
+		public List<IngresoActivoDTO> Data { get; set; }
 
-        public class IngresoActivoDTO
-        {
-            public int Id { get; set; }
+		public class IngresoActivoDTO
+		{
+			public int Id { get; set; }
 
-            [Display(Name = "Correlativo")]
-            public string Correlativo { get; set; }
+			[Display(Name = "Correlativo")]
+			public string Correlativo { get; set; }
+
+			[Display(Name = "Fecha de Ingreso")]
+			public DateTime FechaIngreso { get; set; }
 
 
-            public List<DetalleIngresoActivoDTO> DetalleIngresoActivoDTO { get; set; }
-        }
-    }
+			public List<DetalleIngresoActivoDTO> DetalleIngresoActivoDTO { get; set; }
+		}
+	}
 }
