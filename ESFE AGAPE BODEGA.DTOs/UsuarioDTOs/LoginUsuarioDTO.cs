18a6a741cec3932fe142bf2ca0af1cc135e611c0
Fernando-Jose-Claros-Orellana <ferinocente357@gmail.com>
2024-10-09 10:10:53 -0600

    Se agregan validaciones en login usuario dto

--- ESFE AGAPE BODEGA.DTOs/UsuarioDTOs/LoginUsuarioDTO.cs
+++ ESFE AGAPE BODEGA.DTOs/UsuarioDTOs/LoginUsuarioDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,10 @@ namespace ESFE_AGAPE_BODEGA.DTOs.UsuarioDTOs
 {
     public class LoginUsuarioDTO
     {
+        [Required(ErrorMessage = "El campo DUI es obligatorio")]
+        [RegularExpression(@"^\d{9}$", ErrorMessage = "El DUI debe tener exactamente 9 dígitos.")]
         public string DUI { get; set; }
+        [Required(ErrorMessage = "El campo Password es obligatorio")]
         public string Password { get; set; }
     }
 }
