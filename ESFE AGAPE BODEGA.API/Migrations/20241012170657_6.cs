194faa5176048f20180e76340ad2829e49f95800
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-12 11:13:43 -0600

    Permitir nulos en InventarioActivoId en detalleIngresos
    Se ha añadido una nueva migración que modifica la estructura de la base de datos, específicamente la tabla `detalleIngresos`, para permitir que la columna `InventarioActivoId` acepte valores nulos. Esto incluye:
    
    - Eliminación y reconfiguración de la clave foránea `FK_detalleIngresos_inventarioActivos_InventarioActivoId`.
    - Actualización de la columna `InventarioActivoId` para que sea nullable.
    - Cambios en `ApplicationDbContextModelSnapshot.cs` para reflejar estas modificaciones.

---
+++ ESFE AGAPE BODEGA.API/Migrations/20241012170657_6.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESFE_AGAPE_BODEGA.API.Migrations
+{
+    public partial class _6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_detalleIngresos_inventarioActivos_InventarioActivoId",
+                table: "detalleIngresos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventarioActivoId",
+                table: "detalleIngresos",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_detalleIngresos_inventarioActivos_InventarioActivoId",
+                table: "detalleIngresos",
+                column: "InventarioActivoId",
+                principalTable: "inventarioActivos",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_detalleIngresos_inventarioActivos_InventarioActivoId",
+                table: "detalleIngresos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InventarioActivoId",
+                table: "detalleIngresos",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_detalleIngresos_inventarioActivos_InventarioActivoId",
+                table: "detalleIngresos",
+                column: "InventarioActivoId",
+                principalTable: "inventarioActivos",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
