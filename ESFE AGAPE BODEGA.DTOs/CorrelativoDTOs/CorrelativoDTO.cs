952857abde7365841f9974c62b014a935a364fde
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-21 16:33:13 -0600

    Añadir nueva carpeta y clase CorrelativoDTO
    Se ha añadido un nuevo grupo de elementos en el archivo `ESFE AGAPE BODEGA.BlazorWebAssembly.csproj` para incluir una nueva carpeta denominada `Pages\NewFolder\`.
    
    Se ha creado una nueva clase `CorrelativoDTO` en el archivo `CorrelativoDTO.cs` dentro del espacio de nombres `ESFE_AGAPE_BODEGA.DTOs.CorrelativoDTOs`. Esta clase contiene las siguientes propiedades:
    - `Id` de tipo `int`
    - `Valor` de tipo `int`
    - `AliasInicial` de tipo `string`
    - `AliasFinal` de tipo `string`
    - `Entidad` de tipo `string`

---
+++ ESFE AGAPE BODEGA.DTOs/CorrelativoDTOs/CorrelativoDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESFE_AGAPE_BODEGA.DTOs.CorrelativoDTOs
+{
+	public class CorrelativoDTO
+	{
+		public int Id { get; set; }
+		public int Valor { get; set; }
+		public string AliasInicial { get; set; }
+		public string AliasFinal { get; set; }
+		public string Entidad { get; set; }
+	}
+}
