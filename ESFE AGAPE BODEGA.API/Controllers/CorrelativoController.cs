c33f367c570471fda468630e226df5272a128bb6
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-21 16:31:42 -0600

    Añadir CorrelativoController con métodos CRUD básicos
    Se han añadido varias directivas `using` para importar los espacios de nombres necesarios, incluyendo `ESFE_AGAPE_BODEGA.API.Models.DAL`, `ESFE_AGAPE_BODEGA.API.Models.Entitys`, `ESFE_AGAPE_BODEGA.DTOs.BodegaDTOs`, `ESFE_AGAPE_BODEGA.DTOs.CorrelativoDTOs`, `Microsoft.AspNetCore.Authorization` y `Microsoft.AspNetCore.Mvc`.
    
    Se ha creado un nuevo controlador llamado `CorrelativoController` dentro del espacio de nombres `ESFE_AGAPE_BODEGA.API.Controllers`.
    
    El controlador `CorrelativoController` está decorado con los atributos `[Route("api/[controller]")]`, `[ApiController]` y `[Authorize]`.
    
    Se ha añadido un campo privado `_correlativoDAL` de tipo `CorrelativoDAL` y se ha inicializado a través del constructor del controlador.
    
    Se ha añadido un método `ObtenerCorrelativo` que responde a solicitudes HTTP GET en la ruta `api/Correlativo/{id}`. Este método obtiene un correlativo por su ID y devuelve un `CorrelativoDTO` envuelto en un `OkResult`. Si no se encuentra el correlativo, devuelve un `NotFoundResult`.
    
    Se ha añadido un método `CrearCorrelativo` que responde a solicitudes HTTP POST. Este método crea un nuevo correlativo a partir de un `CrearCorrelativoDTO` y devuelve el correlativo creado envuelto en un `CreatedAtActionResult`.
    
    Se ha añadido un método `EliminarCorrelativo` que responde a solicitudes HTTP DELETE en la ruta `api/Correlativo/{id}`. Este método elimina un correlativo por su ID y devuelve un `NoContentResult`.

---
+++ ESFE AGAPE BODEGA.API/Controllers/CorrelativoController.cs
@@ -0,0 +1,84 @@
+﻿using ESFE_AGAPE_BODEGA.API.Models.DAL;
+using ESFE_AGAPE_BODEGA.API.Models.Entitys;
+using ESFE_AGAPE_BODEGA.DTOs.BodegaDTOs;
+using ESFE_AGAPE_BODEGA.DTOs.CorrelativoDTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ESFE_AGAPE_BODEGA.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	[Authorize]
+	public class CorrelativoController : ControllerBase
+	{
+		private readonly CorrelativoDAL _correlativoDAL;
+
+		public CorrelativoController(CorrelativoDAL correlativoDAL)
+		{
+			_correlativoDAL = correlativoDAL;
+		}
+
+		
+
+		//obtener por id
+		[HttpGet("{id}")]
+		public async Task<ActionResult<CorrelativoDTO>> ObtenerCorrelativo(int id)
+		{
+			var correlativo = await _correlativoDAL.ObtenerCorrelativoPorId(id);
+
+			if (correlativo == null)
+			{
+				return NotFound();  // Devuelve un NotFoundResult
+			}
+
+			var bodegaDTO = new CorrelativoDTO
+			{
+				Id = correlativo.Id,
+				Valor = correlativo.Valor,
+				AliasInicial = correlativo.AliasInicial,
+				AliasFinal = correlativo.AliasFinal,
+				Entidad = correlativo.Entidad
+			};
+
+			return Ok(bodegaDTO);  // Devuelve el objeto envuelto en un OkResult
+		}
+
+
+
+		
+
+		[HttpPost]
+		public async Task<ActionResult<CorrelativoDTO>> CrearCorrelativo(CrearCorrelativoDTO crearCorrelativoDTO)
+		{
+			var nuevoCorrelativo = new Correlativo
+			{
+				Valor = crearCorrelativoDTO.Valor,
+				AliasInicial = crearCorrelativoDTO.AliasInicial,
+				AliasFinal = crearCorrelativoDTO.AliasFinal,
+				Entidad = crearCorrelativoDTO.Entidad
+			};
+
+			var correlativoCreado = await _correlativoDAL.CrearCorrelativo(nuevoCorrelativo);
+
+			var correlativoDTO = new CorrelativoDTO
+			{
+				Id = correlativoCreado.Id,
+				Valor = correlativoCreado.Valor,
+				AliasInicial = correlativoCreado.AliasInicial,
+				AliasFinal = correlativoCreado.AliasFinal,
+				Entidad = correlativoCreado.Entidad
+			};
+
+			return CreatedAtAction(nameof(ObtenerCorrelativo), new { id = correlativoDTO.Id }, correlativoDTO);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> EliminarCorrelativo(int id)
+		{
+			await _correlativoDAL.EliminarCorrelativo(id);
+			return NoContent();
+		}
+
+	}
+}
