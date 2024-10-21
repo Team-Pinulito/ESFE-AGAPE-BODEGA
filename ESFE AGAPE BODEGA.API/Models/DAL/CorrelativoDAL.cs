b13d4113abcb8daa7657ed08044c8caa90b8db4e
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-21 16:32:22 -0600

    Añadir clase CorrelativoDAL con operaciones CRUD
    Se ha añadido una nueva clase `CorrelativoDAL` en el archivo `CorrelativoDAL.cs` para manejar las operaciones CRUD (Crear, Leer, Eliminar) para la entidad `Correlativo` utilizando Entity Framework Core. La clase incluye la inyección de dependencia del contexto de la base de datos `ApplicationDbContext`, un constructor para inicializar `applicationDbContext`, y los métodos `ObtenerCorrelativoPorId`, `CrearCorrelativo` y `EliminarCorrelativo`.

---
+++ ESFE AGAPE BODEGA.API/Models/DAL/CorrelativoDAL.cs
@@ -0,0 +1,45 @@
+﻿using ESFE_AGAPE_BODEGA.API.Models.Entitys;
+using Microsoft.EntityFrameworkCore;
+
+namespace ESFE_AGAPE_BODEGA.API.Models.DAL
+{
+	public class CorrelativoDAL
+	{
+		//injeccion de dependencia de el contexto de la base de datos
+		private readonly ApplicationDbContext applicationDbContext;
+
+		//constructor
+		public CorrelativoDAL(ApplicationDbContext applicationDbContext)
+		{
+			this.applicationDbContext = applicationDbContext;
+		}
+
+
+		public async Task<Correlativo> ObtenerCorrelativoPorId(int id)
+		{
+			return await applicationDbContext.correlativos.FindAsync(id);
+		}
+
+
+
+		public async Task<Correlativo> CrearCorrelativo(Correlativo nuevoCorrelativo)
+		{
+			applicationDbContext.correlativos.Add(nuevoCorrelativo);
+			await applicationDbContext.SaveChangesAsync();
+			return nuevoCorrelativo;
+		}
+
+	
+
+		public async Task EliminarCorrelativo(int id)
+		{
+			var correlativo = await applicationDbContext.correlativos.FindAsync(id);
+			if (correlativo != null)
+			{
+				applicationDbContext.correlativos.Remove(correlativo);
+				await applicationDbContext.SaveChangesAsync();
+			}
+		}
+	}
+}
+
