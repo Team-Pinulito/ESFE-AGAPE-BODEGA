92cd4a9cebaff9134b85fc3426174093c257e1ce
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-21 16:32:45 -0600

    Añadir clase Correlativo en nuevo archivo Correlativo.cs
    Se ha añadido un nuevo archivo llamado `Correlativo.cs` en el espacio de nombres `ESFE_AGAPE_BODEGA.API.Models.Entitys`. Dentro de este archivo, se ha definido una nueva clase llamada `Correlativo` con las propiedades `Id` (int), `Valor` (int), `AliasInicial` (string), `AliasFinal` (string) y `Entidad` (string).

---
+++ ESFE AGAPE BODEGA.API/Models/Entitys/Correlativo.cs
@@ -0,0 +1,12 @@
+﻿namespace ESFE_AGAPE_BODEGA.API.Models.Entitys
+{
+	public class Correlativo
+	{
+		public int Id { get; set; }
+		public int Valor { get; set; }
+		public string AliasInicial { get; set; }
+		public string AliasFinal { get; set; } 
+		public string Entidad { get; set; }
+
+	}
+}
