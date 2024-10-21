1fabd1d4e8137026856c35a3bceb60a2b1ef9362
Eliseo Isai Serrano Guzman <sg24004@esfe.agape.edu.sv>
2024-10-21 10:43:34 -0600

    Añadir nuevas migraciones para cambios en la base de datos
    Se ha generado automáticamente el archivo de migración `20241020045519_8.Designer.cs` que define el modelo de datos y las relaciones entre entidades utilizando Entity Framework Core. Además, se ha añadido el archivo `20241020045519_8.cs` que cambia el tipo de datos de la columna `Correlativo` de la tabla `ingresoactivos` de `string` a `int` en la migración `Up`, y lo revierte en la migración `Down`.
    
    También se ha añadido el archivo de migración `20241020045929_9.Designer.cs` que define el modelo de datos y las relaciones entre entidades, incluyendo la configuración de varias entidades y sus restricciones. El archivo `20241020045929_9.cs` contiene una clase parcial `9` que hereda de `Migration` con métodos `Up` y `Down` vacíos, listos para futuros cambios.

---
+++ ESFE AGAPE BODEGA.API/Migrations/20241020045519_8.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESFE_AGAPE_BODEGA.API.Migrations
+{
+    public partial class _8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Correlativo",
+                table: "ingresoactivos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Correlativo",
+                table: "ingresoactivos",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
---
+++ ESFE AGAPE BODEGA.API/Migrations/20241020045929_9.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ESFE_AGAPE_BODEGA.API.Migrations
+{
+    public partial class _9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
